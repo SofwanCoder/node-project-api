5c5a91cd62dd066859a2b7200e304d7cd25d67c0
Prince Sofwan <sofwancoder@gmail.com>
2022-03-08 01:53:49 +0100

    refactor: index

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,13 @@ import { createServer } from "http";
 import { config } from "dotenv";
 config();
 import { app } from "./app";
-import seed from "./seed";
 // import { sequelize } from "./sequelize";
 const port = process.env.PORT || 3000;
 
 void (async () => {
-  // await sequelize.query("SET FOREIGN_KEY_CHECKS = 0");
   // await sequelize.sync({
   //   logging: console.log,
   // });
-  await seed();
-  // await sequelize.query("SET FOREIGN_KEY_CHECKS = 1");
   await import("./jobs");
   createServer(app).listen(port, () =>
     console.info(`Server running on port ${port}`)
