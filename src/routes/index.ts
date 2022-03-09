fecf5bacc36401baa96e7e787bc38c95bb8f7afc
Prince Sofwan <sofwancoder@gmail.com>
2022-03-09 02:24:14 +0100

    refactor: authorization

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import express, { Router } from "express";
+import { decryptAuthorization } from "../middlewares/decryptAuthorization";
 import { routerDocs } from "./docs";
 import { routerVersion1 } from "./v1.routes";
 
 const router = express.Router();
+
+router.use(decryptAuthorization);
 
 router.use("/v1", routerVersion1);
 router.use("/docs", routerDocs);
