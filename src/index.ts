dec844cb2a1778444781553092323af8cf1a4afa
Prince Sofwan <sofwancoder@gmail.com>
2022-05-23 01:22:17 +0100

    chore: graceful server shutdown

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
 import { createServer } from "http";
 import { config } from "dotenv";
 config();
+import { onShutdown } from "node-graceful-shutdown";
 import { app } from "./app";
-// import { sequelize } from "./sequelize";
+import { sequelize } from "./sequelize";
 const port = process.env.PORT || 3000;
 
+const server = createServer(app);
+
 void (async () => {
-  // await sequelize.sync({
-  //   logging: console.log,
-  // });
+  await sequelize.authenticate();
   await import("./jobs");
-  createServer(app).listen(port, () =>
+  server.listen(port, () =>
     console.info(`Server running on port ${port}`)
   );
 })();
+
+
+onShutdown(async () => {
+  console.log("Closing http server.");
+  await new Promise(resolve => {
+    server.close(resolve);
+  })
+  console.log("Closing Database connection.");
+  await sequelize.close();
+})
