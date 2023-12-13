bd640419fe406d421c01e9e3034a838dfac3cd4f
Sofwan Lawal <sofwancoder@gmail.com>
2023-12-13 17:39:48 +0100

    chore: improve code maintenance

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:20-alpine as node
+FROM node:20-alpine as builder
 RUN mkdir -p /app/node_modules && chown -R node:node /app
 WORKDIR /app
 COPY --chown=node:node package*.json ./
@@ -6,6 +6,18 @@ USER node
 RUN npm install
 COPY --chown=node:node . .
 RUN npm run build
+RUN npx tsc migrations/* --outDir dist/migrations
+RUN npx tsc knexfile.ts --outDir dist
+
+FROM node:20-alpine as runner
+RUN mkdir -p /app/node_modules && chown -R node:node /app
+WORKDIR /app
+USER node
+COPY --from=builder /app/package*.json ./
+COPY --from=builder /app/dist ./dist
+COPY --from=builder /app/public ./public
+RUN npm install --omit=dev
+RUN rm -rf `npm config get cache`
 EXPOSE 8080
 ENV NODE_ENV=production
-ENTRYPOINT ["npm", "start"]
+ENTRYPOINT ["npm", "run", "start:live"]
--- src/emails/auth.email.ts
+++ src/emails/auth.email.ts
@@ -4,28 +4,25 @@ import { compile } from "ejs";
 import juice from "juice";
 import { User } from "../models/User";
 import Verifier from "../models/Verifier";
-import config from "../config";
 
-const _resetPasswordTemplate = compile(
-  readFileSync(join(config.dir.views, "emails/password.reset.ejs")).toString(),
-  {
-    views: [join(config.dir.views, "emails")],
-  },
-);
+const _resetPasswordTemplate = getTemplate("password.reset");
 
-const _verifyEmailTemplate = compile(
-  readFileSync(join(config.dir.views, "emails/user.verify.ejs")).toString(),
-  {
-    views: [join(config.dir.views, "emails")],
-  },
-);
+const _verifyEmailTemplate = getTemplate("user.verify");
 
 export function resetPasswordTemplate(user: User, verifier: Verifier) {
-  const result = _resetPasswordTemplate({ user, verifier });
-  return juice(result);
+  return _resetPasswordTemplate({ user, verifier });
 }
 
 export function verifyUserTemplate(user: User, verifier: Verifier) {
-  const result = _verifyEmailTemplate({ user, verifier });
-  return juice(result);
+  return _verifyEmailTemplate({ user, verifier });
+}
+
+function getTemplate(template: string) {
+  const content = readFileSync(
+    join(__dirname, `templates/${template}.ejs`),
+  ).toString();
+  const juiced = juice(content);
+  return compile(juiced, {
+    views: [join(__dirname, "templates")],
+  });
 }
